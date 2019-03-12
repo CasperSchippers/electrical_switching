d5e075ffa1eb93435fbeae1bdb27a8e31ceb4819
Casper Schippers <c.f.schippers@tue.nl>
2019-03-12 12:05:03 +0100

    Added measurement date and version

--- electrical_switching/electrical_switching.py
+++ electrical_switching/electrical_switching.py
@@ -8,9 +8,21 @@ from pymeasure.display.windows import ManagedWindow
 from pymeasure.experiment import Procedure, Results, unique_filename
 from pymeasure.experiment import Parameter, FloatParameter, BooleanParameter
 
+from datetime import datetime
+from git import cmd, Repo, exc
+
+try:
+    version = cmd.Git(Repo(search_parent_directories=True)).describe()
+except exc.GitCommandError:
+    version = 'none'
+
+date = datetime.now()
+
 
 class MeasurementProcedure(Procedure):
     # Define parameters
+    AAA = Parameter('Software version', default=version)
+    AAB = Parameter('Measurement date', default=date)
 
     # Define start-up sequence
     def startup(self):
@@ -28,7 +40,8 @@ class MeasurementProcedure(Procedure):
 class MainWindow(ManagedWindow):
     def __init__(self):
         super(MainWindow, self).__init__(
-            procedure_class=MeasurementProcedure)
+            procedure_class=MeasurementProcedure,
+        )
 
 
 if __name__ == "__main__":
